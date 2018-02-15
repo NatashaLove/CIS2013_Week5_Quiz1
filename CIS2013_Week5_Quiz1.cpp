d30ff6f535db96ffbf628744514ce468b940a842
Natasha S Love <debora@inbox.ru>
2018-02-14 20:02:22 -0600

    functions with prime numbers and lcm

--- CIS2013_Week5_Quiz1.cpp
+++ CIS2013_Week5_Quiz1.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <cmath>
 #include <stdlib.h>
@@ -6,30 +7,32 @@
 using namespace std;
 void get_input (int& num_1, int& num_2);
 void prime_num (int& num_1, int& num_2);
-void get_lcm (int one, int two);
+int get_lcm (int one, int two);
 
 int main (){
 
 	srand(time(NULL)); 
 	int num1, num2;
+	int lcm;
+	
 	
 	get_input (num1, num2);
+	lcm = get_lcm (num1, num2);
 	prime_num (num1, num2);
-	get_lcm (num1, num2);
+	
 	
 	return 0;
 }
 	
 void get_input (int& num_1, int& num_2) {
 	
-	using namespace std;
 	
 	cout << "Give me two numbers \n";
 		cin >> num_1;
 		cin >> num_2;
 		
 }
-void get_lcm (int one, int two)
+int get_lcm (int one, int two)
 {
 	int one_counter = one;
 	int two_counter = two;
@@ -43,6 +46,8 @@ void get_lcm (int one, int two)
 		}
 	}
 	cout << "The LCM is " << two << endl;
+	return two;
+	//also can put return one;
 }
 
 	void prime_num (int& num_1, int& num_2){
